3b758c2c49ed5dd4371d81e080dcbdaf774877b2
Henry Le <lethehien18@gmail.com>
2020-02-13 16:49:01 -0800

    update
    go

--- workspace/L2_AlgorithmLab/src/modules/RecModule.java
+++ workspace/L2_AlgorithmLab/src/modules/RecModule.java
@@ -5,24 +5,45 @@ public class RecModule {
 	public static int binarySearch(int[] list, int target) {
 		return binarySearch(list, target, 0, list.length - 1);
 	}
-	
+
 	private static int binarySearch(int[] list, int target, int min, int max) {
-		return 0;
+		if (max >= min) {
+			int mid = min + (max - min) / 2;
+
+			if (list[mid] == target) {
+				return mid;
+			}
+
+			if (list[mid] > target) {
+				return binarySearch(list, target, min, mid - 1);
+			}
+
+			return binarySearch(list, target, mid + 1, max);
+		}
+
+		return -1;
 	}
-	
-	
+
 	public static void selectionSort(int[] list) {
-		selectionSort(list, 0);			//sort entire data
+		selectionSort(list, 0); // sort entire data
 	}
-	
 
 	private static void selectionSort(int[] list, int start) {
-		
-		
+		if (start >= list.length - 1)
+			return;
+		int minIndex = start;
+		for (int index = start + 1; index < list.length; index++) {
+			if (list[index] < list[minIndex])
+				minIndex = index;
+		}
+		swap(list, start, minIndex);
+		selectionSort(list, start + 1);
 	}
-	
+
 	public static void swap(int[] list, int first, int second) {
-		
+		int temp = list[first];
+		list[first] = list[second];
+		list[second] = temp;
 	}
-	
+
 }
